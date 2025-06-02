422f23c370a7ea68c795c8342007c78bdb4bbabd
Kriti <kritiarora0810@gmail.com>
2025-06-02 12:58:50 +0530

    latest backend commit

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,1 +1,4 @@
-export const BASE_URL = 'http://localhost:8000/';
+// export const BASE_URL = 'http://localhost:8000/';
+
+export const BASE_URL = 'https://3a49-2405-201-5016-709a-a563-54ee-e3e7-9a63.ngrok-free.app/';
+
--- frontend/src/config.jsx
+++ frontend/src/config.jsx
@@ -1,4 +1,5 @@
 // export const BASE_URL = 'https://4596-2401-4900-1c73-3dc-1459-bf6-76e7-baf7.ngrok-free.app/';
 
-export const BASE_URL = 'http://localhost:8000/';
+// export const BASE_URL = 'http://localhost:8000/';
 
+export const BASE_URL = 'https://3a49-2405-201-5016-709a-a563-54ee-e3e7-9a63.ngrok-free.app/';
